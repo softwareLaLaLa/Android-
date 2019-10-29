77bd71a313e907d176094592f4f3436a9bb2da22
SongLiang <1173646513@qq.com>
2019-10-29 20:46:10 +0800

    no message

--- app/src/main/java/com/example/lalala/MainActivity.java
+++ app/src/main/java/com/example/lalala/MainActivity.java
@@ -16,20 +16,17 @@
 package com.example.lalala;
 
 import android.os.Bundle;
+import android.view.Menu;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
