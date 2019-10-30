7304e82e8ca60194af57c4dd23c7e041dce657a9
SongLiang <1173646513@qq.com>
2019-10-30 21:04:35 +0800

    修改主页

--- app/src/main/java/com/example/lalala/MainActivity.java
+++ app/src/main/java/com/example/lalala/MainActivity.java
@@ -2,14 +2,37 @@ package com.example.lalala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.lalala.sort.SortActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnSort;
+    private Button btnOther;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSort=findViewById(R.id.btn_sort);
+        btnOther=findViewById(R.id.btn_user);
+        btnSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SortActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnOther.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/lalala/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/lalala/ui/home/HomeFragment.java
@@ -27,21 +27,7 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        //final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-        btnSort = root.findViewById(R.id.btn_sort);
-        btnSort.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SortActivity.class);
-                startActivity(intent);
-            }
-        });
+
         gridView = root.findViewById(R.id.grid_follow_field);
         gridView.setAdapter(new FollowFieldAdapter(getActivity()));
 
